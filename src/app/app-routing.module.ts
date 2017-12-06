daa74c6fbdd635db7544a9ace3428d8b078ca736
Guillaume POTIER <gpotier@rs2i.fr>
2017-12-06 18:36:31 +0100

    fix bug init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  imports: [
+      RouterModule.forRoot(
+          appRoutes
+      )
+  ],
+  exports: [
+      RouterModule
+  ]
 })
 export class AppRoutingModule { }
